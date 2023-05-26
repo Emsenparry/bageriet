836cd3b6767b5674017b099d5cc2732fdbb1858e
Emma Nielsen <Emmanielsen843@gmail.com>
2023-05-26 10:42:25 +0200

    text area and news updates

--- src/Components/TextArea/TextArea.jsx
+++ src/Components/TextArea/TextArea.jsx
@@ -1,15 +1,18 @@
-import React from 'react'
+import React from "react";
+import './TextArea.scss';
 
 const TextAreaOne = () => {
   return (
-    <div>
-        <h3>Vi skaber lækkert brød!</h3>
-        <p>
-        Der er mange tilgængelige udgaver af Lorem Ipsum, men de fleste udgaver har gennemgået forandringer, når nogen har <br />
+    <div className="centered-content">
+      <h3>Vi skaber lækkert brød!</h3>
+      <p>
+        Der er mange tilgængelige udgaver af Lorem Ipsum, men de fleste udgaver
+        har gennemgået forandringer, når nogen har
+        <br />
         tilføjet humor eller tilfældige ord, som på ingen måde ser ægte ud.
-        </p>
+      </p>
     </div>
-  )
-}
+  );
+};
 
-export { TextAreaOne }
+export { TextAreaOne };
